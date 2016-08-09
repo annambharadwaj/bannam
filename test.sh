3fc3b2347c16da960c25ee1853424dd9bba00b79
annambharadwaj <annambharadwaj@gmail.com>
2016-08-09 18:02:09 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@ echo "Testing"
 echo "Argument one is $a"
 echo "Argument two is $b"
 echo "Argument three is $c"
-$a == 30
-$b == 03
-$c == 8990
+$a = 30
+$b = 03
+$c = 8990
 sum=(($a + $b + $c))
 echo "Total is $sum"
